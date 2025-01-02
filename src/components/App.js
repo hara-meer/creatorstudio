ddbccda6afa90a7328f18be5235aa22286213aef
papepopopy <hapohee@naver.com>
2025-01-02 13:48:47 +0900

    250102 main-style

--- src/components/App.js
+++ src/components/App.js
@@ -98,7 +98,7 @@ function App() {
         <h2>크리에이터 스튜디오</h2>
         <p className="add-text">부산광역시 동래구 사직로 77 (사직동, 부산광역시 체육회관)</p>
         <ul className="box1-slide">
-          <li className="active" style={{ backgroundImage: 'url("../images/main_bg01.jpg")' }}>
+          <li className="active" style={{ backgroundImage: 'url("../image/img_01.jpg")' }}>
             <div className="text-box">
               <h3 className="box1-title">
                 창작실 3
@@ -106,7 +106,7 @@ function App() {
               <p>소규모 회의 및 모임 공간입니다. 최대 8명까지 수용할 수 있으며, 화이트보드 사용이 가능합니다.</p>
             </div>
           </li>
-          <li>
+          <li style={{ backgroundImage: 'url("../image/img_02.jpg")' }}>
             <div className="text-box">
               <h3 className="box1-title">
                 창작실 2
@@ -114,7 +114,7 @@ function App() {
               <p>소규모 회의 및 모임 공간입니다. 최대 8명까지 수용할 수 있으며, 화이트보드 사용이 가능합니다.</p>
             </div>
           </li>
-          <li>
+          <li style={{ backgroundImage: 'url("../image/img_03.jpg")' }}>
             <div className="text-box">
               <h3 className="box1-title">
                 창작실 1
@@ -165,8 +165,8 @@ function App() {
               <p>27</p>
             </div>
             <div className="box3-text">
-              <h3>실내 마스크 착용 권고 안내</h3>
-              <p>실내 마스크 착용 권고 안내입니다. 실내 마스크 착용, 서로를 위한 배려입니다. 2023년 1월 30일 부터, 실내 마스크 착용 의무 조정 1단계 시행으로 인해 모두가 마스크 착용 후 참여 부탁드립니다.</p>
+              <h3>2023년 설 연휴 운영 안내</h3>
+              <p>2023년 설 연휴 운영 안내입니다.</p>
               <div className="">View more</div>
             </div>
             <div className="box3-img">
@@ -179,8 +179,8 @@ function App() {
               <p>27</p>
             </div>
             <div className="box3-text">
-              <h3>실내 마스크 착용 권고 안내</h3>
-              <p>실내 마스크 착용 권고 안내입니다. 실내 마스크 착용, 서로를 위한 배려입니다. 2023년 1월 30일 부터, 실내 마스크 착용 의무 조정 1단계 시행으로 인해 모두가 마스크 착용 후 참여 부탁드립니다.</p>
+              <h3>흑토끼의 해, 2023년 계묘년 공지</h3>
+              <p>흑토끼의 해, 2023년 계묘년 공지입니다.</p>
               <div className="">View more</div>
             </div>
             <div className="box3-img">
@@ -190,40 +190,41 @@ function App() {
         </ul>
       </div>
 
-      <div className="sub-box4 innerbox">
-        <h2>프로그램</h2>
-        <div className="box4-teb-box">
-          <div>진행중인 프로그램</div>
-          <div>마감된 프로그램</div>
+      <div className="sub-box4">
+        <div className="innerbox">
+          <h2>프로그램</h2>
+          <div className="box4-teb-box">
+            <div>진행중인 프로그램</div>
+            <div>마감된 프로그램</div>
+          </div>
+          <ul className="box4-btn">
+            <li><img src="./public/image/news_img01" alt="program"/></li>
+            <li><img src="./public/image/news_img01" alt="program"/></li>
+            <li><img src="./public/image/news_img01" alt="program"/></li>
+          </ul>
+          <ul className="box4-board">
+            <li>
+              <div className="img-box"></div>
+              <p>[이벤트]2023 계묘년 EVENT</p>
+            </li>
+            <li>
+              <div className="img-box"></div>
+              <p>2023 부산 콘텐츠코리아 랩 온라인 기획</p>
+            </li>
+            <li>
+              <div className="img-box"></div>
+              <p>원데이 클래스 &lt;블로그 수익화 나도 낼수 있을까?&gt;</p>
+            </li>
+            <li>
+              <div className="img-box"></div>
+              <p>[세미나 및 교육]크리에이트 스튜디오 오프라인 장비교육</p>
+            </li>
+            <li>
+              <div className="img-box"></div>
+              <p>[이벤트]부산 콘텐츠코리아 랩 숏폼 댓글 이벤트</p>
+            </li>
+          </ul>
         </div>
-        <ul className="box4-btn">
-          <li><img src="./public/image/news_img01" alt="program"/></li>
-          <li><img src="./public/image/news_img01" alt="program"/></li>
-          <li><img src="./public/image/news_img01" alt="program"/></li>
-        </ul>
-
-        <ul className="box4-board">
-          <li>
-            <div className="img-box"></div>
-            <p>[이벤트]2023 계묘년 EVENT</p>
-          </li>
-          <li>
-            <div className="img-box"></div>
-            <p>2023 부산 콘텐츠코리아 랩 온라인 기획</p>
-          </li>
-          <li>
-            <div className="img-box"></div>
-            <p>원데이 클래스 &lt;블로그 수익화 나도 낼수 있을까?&gt;</p>
-          </li>
-          <li>
-            <div className="img-box"></div>
-            <p>[세미나 및 교육]크리에이트 스튜디오 오프라인 장비교육</p>
-          </li>
-          <li>
-            <div className="img-box"></div>
-            <p>[이벤트]부산 콘텐츠코리아 랩 숏폼 댓글 이벤트</p>
-          </li>
-        </ul>
       </div>
 
       <div className="sub-box5 innerbox">
